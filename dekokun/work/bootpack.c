70502512d4b08b309e592be332f18c414888950c
dekokun <dekokun@hatena.ne.jp>
2018-01-28 20:49:00 +0900

    4日目開始

--- dekokun/work/bootpack.c
+++ dekokun/work/bootpack.c
@@ -1,15 +1,16 @@
-/* 他のファイルで作った関数がありますとCコンパイラに教える */
-
 void io_hlt(void);
+void write_mem8(int addr, int data);
 
-/* 関数宣言なのに、{}がなくていきなり;を書くと、
-	他のファイルにあるからよろしくね、という意味になるのです。 */
 
 void HariMain(void)
 {
+	int i; /* 変数宣言。iという変数は、32ビットの整数型 */
 
-fin:
-	io_hlt(); /* これでnaskfunc.nasの_io_hltが実行されます */
-	goto fin;
+	for (i = 0xa0000; i <= 0xaffff; i++) {
+		write_mem8(i, i & 0x0f);
+	}
 
+	for (;;) {
+		io_hlt();
+	}
 }
