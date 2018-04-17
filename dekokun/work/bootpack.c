5adb2ad76b266cfaca113750e02a9a4311d148c9
dekokun <dekokun@hatena.ne.jp>
2018-04-17 21:38:29 +0900

    4日目終了

--- dekokun/work/bootpack.c
+++ dekokun/work/bootpack.c
@@ -1,16 +1,115 @@
-void io_hlt(void);
-void write_mem8(int addr, int data);
+#define COL8_000000 0
+#define COL8_FF0000 1
+#define COL8_00FF00 2
+#define COL8_FFFF00 3
+#define COL8_0000FF 4
+#define COL8_FF00FF 5
+#define COL8_00FFFF 6
+#define COL8_FFFFFF 7
+#define COL8_C6C6C6 8
+#define COL8_840000 9
+#define COL8_008400 10
+#define COL8_848400 11
+#define COL8_000084 12
+#define COL8_840084 13
+#define COL8_008484 14
+#define COL8_848484 15
 
+void io_hlt(void);
+void io_cli(void);
+void io_out8(int port, int data);
+int io_load_eflags(void);
+void io_store_eflags(int eflags);
+
+/* 実は同じソースファイルに書いてあっても、定義する前に使うのなら、
+	やっぱり宣言しておかないといけない。 */
+
+void init_palette(void);
+void set_palette(int start, int end, unsigned char *rgb);
+void boxfill(unsigned char *vram, int xsize, unsigned char c, int x0, int y0, int x1, int y1);
 
 void HariMain(void)
 {
-	int i; /* 変数宣言。iという変数は、32ビットの整数型 */
+	unsigned char *vram; /* pという変数は、BYTE [...]用の番地 */
+    int xsize, ysize;
+    xsize = 320;
+    ysize = 200;
 
-	for (i = 0xa0000; i <= 0xaffff; i++) {
-		write_mem8(i, i & 0x0f);
-	}
+    init_palette(); /* パレットを設定 */
 
-	for (;;) {
-		io_hlt();
+    vram = (unsigned char *) 0xa0000; /* 番地を代入 */
+
+    boxfill(vram, xsize, COL8_008484,         0,          0,  xsize - 1, ysize - 29);
+    boxfill(vram, xsize, COL8_C6C6C6,         0, ysize - 28,  xsize - 1, ysize - 28);
+    boxfill(vram, xsize, COL8_FFFFFF,         0, ysize - 27,  xsize - 1, ysize - 27);
+    boxfill(vram, xsize, COL8_C6C6C6,         0, ysize - 26,  xsize - 1, ysize -  1);
+
+    boxfill(vram, xsize, COL8_FFFFFF,         3, ysize - 24,         59, ysize - 24);
+    boxfill(vram, xsize, COL8_FFFFFF,         2, ysize - 24,          2, ysize -  4);
+    boxfill(vram, xsize, COL8_848484,         3, ysize -  4,         59, ysize -  4);
+    boxfill(vram, xsize, COL8_848484,        59, ysize - 23,         59, ysize -  5);
+    boxfill(vram, xsize, COL8_000000,         2, ysize -  3,         59, ysize -  3);
+    boxfill(vram, xsize, COL8_000000,        60, ysize - 24,         60, ysize -  3);
+
+    boxfill(vram, xsize, COL8_848484,xsize - 47, ysize - 24,  xsize - 4, ysize - 24);
+    boxfill(vram, xsize, COL8_848484,xsize - 47, ysize - 23, xsize - 47, ysize -  4);
+    boxfill(vram, xsize, COL8_848484,xsize - 47, ysize -  3, xsize -  4, ysize -  3);
+    boxfill(vram, xsize, COL8_848484,xsize -  3, ysize - 24, xsize -  3, ysize -  3);
+
+    for (;;) {
+        io_hlt();
+    }
+}
+
+void boxfill(unsigned char *vram, int xsize, unsigned char c, int x0, int y0, int x1, int y1)
+{
+    int x, y;
+    for (y=y0; y<= y1; y++) {
+        for (x = x0; x <= x1; x++) {
+            vram[y * xsize + x] = c;
+        }
+    }
+    return;
+}
+
+void init_palette(void)
+{
+	static unsigned char table_rgb[16 * 3] = {
+		0x00, 0x00, 0x00,	/*  0:黒 */
+		0xff, 0x00, 0x00,	/*  1:明るい赤 */
+		0x00, 0xff, 0x00,	/*  2:明るい緑 */
+		0xff, 0xff, 0x00,	/*  3:明るい黄色 */
+		0x00, 0x00, 0xff,	/*  4:明るい青 */
+		0xff, 0x00, 0xff,	/*  5:明るい紫 */
+		0x00, 0xff, 0xff,	/*  6:明るい水色 */
+		0xff, 0xff, 0xff,	/*  7:白 */
+		0xc6, 0xc6, 0xc6,	/*  8:明るい灰色 */
+		0x84, 0x00, 0x00,	/*  9:暗い赤 */
+		0x00, 0x84, 0x00,	/* 10:暗い緑 */
+		0x84, 0x84, 0x00,	/* 11:暗い黄色 */
+		0x00, 0x00, 0x84,	/* 12:暗い青 */
+		0x84, 0x00, 0x84,	/* 13:暗い紫 */
+		0x00, 0x84, 0x84,	/* 14:暗い水色 */
+		0x84, 0x84, 0x84	/* 15:暗い灰色 */
+	};
+	set_palette(0, 15, table_rgb);
+	return;
+
+	/* static char 命令は、データにしか使えないけどDB命令相当 */
+}
+
+void set_palette(int start, int end, unsigned char *rgb)
+{
+	int i, eflags;
+	eflags = io_load_eflags();	/* 割り込み許可フラグの値を記録する */
+	io_cli(); 					/* 許可フラグを0にして割り込み禁止にする */
+	io_out8(0x03c8, start);
+	for (i = start; i <= end; i++) {
+		io_out8(0x03c9, rgb[0] / 4);
+		io_out8(0x03c9, rgb[1] / 4);
+		io_out8(0x03c9, rgb[2] / 4);
+		rgb += 3;
 	}
+	io_store_eflags(eflags);	/* 割り込み許可フラグを元に戻す */
+	return;
 }
--- dekokun/work/naskfunc.nas
+++ dekokun/work/naskfunc.nas
@@ -6,7 +6,10 @@
 [BITS 32]						; 32ビットモード用の機械語を作らせる
 [FILE "naskfunc.nas"]			; ソースファイル名情報
 
-		GLOBAL	_io_hlt,_write_mem8
+		GLOBAL	_io_hlt, _io_cli, _io_sti, _io_stihlt
+		GLOBAL	_io_in8,  _io_in16,  _io_in32
+		GLOBAL	_io_out8, _io_out16, _io_out32
+		GLOBAL	_io_load_eflags, _io_store_eflags
 
 [SECTION .text]
 
@@ -14,8 +17,61 @@ _io_hlt:	; void io_hlt(void);
 		HLT
 		RET
 
-_write_mem8:	; void write_mem8(int addr, int data);
-		MOV		ECX,[ESP+4]		; [ESP+4]にaddrが入っているのでそれをECXに読み込む
-		MOV		AL,[ESP+8]		; [ESP+8]にdataが入っているのでそれをALに読み込む
-		MOV		[ECX],AL
+_io_cli:	; void io_cli(void);
+		CLI
+		RET
+
+_io_sti:	; void io_sti(void);
+		STI
+		RET
+
+_io_stihlt:	; void io_stihlt(void);
+		STI
+		HLT
+		RET
+
+_io_in8:	; int io_in8(int port);
+		MOV		EDX,[ESP+4]		; port
+		MOV		EAX,0
+		IN		AL,DX
+		RET
+
+_io_in16:	; int io_in16(int port);
+		MOV		EDX,[ESP+4]		; port
+		MOV		EAX,0
+		IN		AX,DX
+		RET
+
+_io_in32:	; int io_in32(int port);
+		MOV		EDX,[ESP+4]		; port
+		IN		EAX,DX
+		RET
+
+_io_out8:	; void io_out8(int port, int data);
+		MOV		EDX,[ESP+4]		; port
+		MOV		AL,[ESP+8]		; data
+		OUT		DX,AL
+		RET
+
+_io_out16:	; void io_out16(int port, int data);
+		MOV		EDX,[ESP+4]		; port
+		MOV		EAX,[ESP+8]		; data
+		OUT		DX,AX
+		RET
+
+_io_out32:	; void io_out32(int port, int data);
+		MOV		EDX,[ESP+4]		; port
+		MOV		EAX,[ESP+8]		; data
+		OUT		DX,EAX
+		RET
+
+_io_load_eflags:	; int io_load_eflags(void);
+		PUSHFD		; PUSH EFLAGS という意味
+		POP		EAX
+		RET
+
+_io_store_eflags:	; void io_store_eflags(int eflags);
+		MOV		EAX,[ESP+4]
+		PUSH	EAX
+		POPFD		; POP EFLAGS という意味
 		RET
