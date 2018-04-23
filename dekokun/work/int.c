d52ca2c4488ab9c6505106ff06ed3689488ea821
dekokun <dekokun@hatena.ne.jp>
2018-04-23 09:21:41 +0900

    割り込み処理を減らした後バッファを長くする

--- dekokun/work/int.c
+++ dekokun/work/int.c
@@ -27,17 +27,21 @@ void init_pic(void)
 
 #define PORT_KEYDAT		0x0060
 
+struct KEYBUF keybuf;
+
 void inthandler21(int *esp)
 {
-	struct BOOTINFO *binfo = (struct BOOTINFO *) ADR_BOOTINFO;
-	unsigned char data, s[4];
+	unsigned char data;
 	io_out8(PIC0_OCW2, 0x61);	/* IRQ-01受付完了をPICに通知 */
 	data = io_in8(PORT_KEYDAT);
-
-	sprintf(s, "%02X", data);
-	boxfill8(binfo->vram, binfo->scrnx, COL8_008484, 0, 16, 15, 31);
-	putfonts8_asc(binfo->vram, binfo->scrnx, 0, 16, COL8_FFFFFF, s);
-
+	if (keybuf.len < 32) {
+		keybuf.data[keybuf.next_w] = data;
+		keybuf.len++;
+		keybuf.next_w++;
+		if (keybuf.next_w == 32) {
+			keybuf.next_w = 0;
+		}
+	}
 	return;
 }
 
