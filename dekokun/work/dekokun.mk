614fe7263974aae5d51b6bafe5bc8de117efd38a
dekokun <dekokun@hatena.ne.jp>
2018-06-17 21:19:42 +0900

    ディレクトリに別れたのディレクトリごとコピーする

--- dekokun/work/dekokun.mk
+++ dekokun/work/dekokun.mk
@@ -3,6 +3,6 @@ DIR ?= harib04a
 
 .PHONY: cp
 cp:
-	cp -f ../../$(DAY)_day/$(DIR)/* ./
+	cp -rf ../../$(DAY)_day/$(DIR)/* ./
 	nkf --in-place -Lu -w *.c *.nas *.h Makefile
 	echo include dekokun.mk >> Makefile
