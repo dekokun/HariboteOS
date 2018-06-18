14725f75d29435db359433220354b3423b2907fc
dekokun <dekokun@hatena.ne.jp>
2018-06-18 16:35:18 +0900

    前回のコミットで改行コードと文字コードがずれていたので修正

--- dekokun/work/dekokun.mk
+++ dekokun/work/dekokun.mk
@@ -4,5 +4,5 @@ DIR ?= harib04a
 .PHONY: cp
 cp:
 	cp -rf ../../$(DAY)_day/$(DIR)/* ./
-	nkf --in-place -Lu -w *.c *.nas *.h Makefile
+	nkf --in-place -Lu -w */*.c */*.nas */*.h Makefile
 	echo include dekokun.mk >> Makefile
