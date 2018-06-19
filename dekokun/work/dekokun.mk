7e3bb31cb4863298fac3bd1831f66b7bd2b592d7
dekokun <dekokun@hatena.ne.jp>
2018-06-19 15:30:47 +0900

    やはりUTF-8がいい

--- dekokun/work/dekokun.mk
+++ dekokun/work/dekokun.mk
@@ -4,5 +4,5 @@ DIR ?= harib04a
 .PHONY: cp
 cp:
 	cp -rf ../../$(DAY)_day/$(DIR)/* ./
-	nkf --in-place -Lu -s */*.c */*.nas */*.h Makefile */Makefile
+	nkf --in-place -Lu -w */*.c */*.nas */*.h Makefile */Makefile
 	echo include dekokun.mk >> Makefile
